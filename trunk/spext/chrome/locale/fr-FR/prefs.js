dba2db1fe2695aa94efc163605bb7bc1bedd99cf
capi <capi@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-01-16 10:19:26 +0000

    [SPExt] Dépot du projet

---
+++ trunk/spext/chrome/locale/fr-FR/prefs.js
@@ -0,0 +1,20 @@
+/**
+ * @author Unibozu
+ * @license GNU/GPL
+ */
+
+Ximplements(Xlocales, {
+	'title': 'Options Xtense v.$1', // Toolbar version 
+	'onclick handle-messages': 'Seuls les messages sélectionnés sont envoyés. Si vous cochez cette option, aucun message ne sera envoyé. Etes-vous sûr de vouloir continuer ?',
+	'onclick msg-only_checked': 'L\'envoi automatique des messages est activé. Si vous cochez cette option, aucun message ne sera envoyé. Etes-vous sûr de vouloir continuer ?',
+	'onclick dev': 'Le mode de developpement permet d\'avoir accès a des outils spéciaux pour les developpeurs. En tant que simple usager vous n\'en avez pas besoin. Etes-vous certain de vouloi l\'activer ?',
+	
+	'save end': '\nCliquez sur OK pour modifier les informations ou sur Annuler pour quitter sans enregistrer',
+	'save fill': 'Vous devez remplir tous les champs du serveur OGSpy n°$1', // Server number
+	'save wrong universe': 'L\'url de l\'univers du serveur n°$1 est invalide, il doit être de cette forme : http://uniX.ogame.fr ou http://betaX.e-univers.org (en remplaçant X par le numéro de votre univers)', // Server number
+	'save wrong user': 'Le pseudo n°$1 est invalide, il doit contenir entre 3 et 15 caractères alphanumériques ainsi que des tirets, des espaces et des underscores (- _)', // Server number
+	'save wrong password': 'Le mot de passe n°$1 est invalide, il doit contenir entre 6 et 15 caractères alphanumériques ainsi que des tirets, des espaces et des underscores)', // Server number
+	'save wrong url old': 'L`\'url du plugin du serveur OGSpy n°$1 est invalide, vous avez saisis l\'url d\'une ancienne version du plugin', // Server number
+	'save wrong url': 'L`\'url du plugin du serveur OGSpy n°$1 est invalide, il doit s\'agir d\'une URL valide (ex: http://domaine.fr/mod/Xtense/xtense.php ou http://domaine.fr/xtense.php', // Server number
+	'save unavailable parser': 'Serveur OGSpy n°$1 : le support de la langue ($2) du serveur ogame n\'est pas disponible',
+});
---
+++ trunk/spext/defaults/preferences/spgdbtoolajax.js
@@ -0,0 +1,18 @@
+//--- BDD ---//
+pref("spgdbtoolajax.root", '');
+pref("spgdbtoolajax.username", '');
+pref("spgdbtoolajax.password", '');
+pref("spgdbtoolajax.galaxy.server", '');
+
+//--- Cdr ---//
+pref("spgdbtoolajax.ColorierCDR.col0", 'red');
+pref("spgdbtoolajax.ColorierCDR.col1", 'orange');
+pref("spgdbtoolajax.ColorierCDR.col2", 'yellow');
+pref("spgdbtoolajax.ColorierCDR.col3", 'green');
+pref("spgdbtoolajax.ColorierCDR.col4", 'cyan');
+pref("spgdbtoolajax.ColorierCDR.seuil1", 10000);
+pref("spgdbtoolajax.ColorierCDR.seuil2", 100000);
+pref("spgdbtoolajax.ColorierCDR.seuil3", 1000000);
+pref("spgdbtoolajax.ColorierCDR.seuil4", 10000000);
+
+//--- General ---/
