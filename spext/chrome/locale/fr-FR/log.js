6f95820524a4131ef4036eae9bc8bf1745e300b0
capi <capi@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-01-16 10:19:26 +0000

    [SPExt] Dépot du projet

---
+++ spext/chrome/locale/fr-FR/log.js
@@ -0,0 +1,30 @@
+/**
+ * @author Unibozu
+ * @license GNU/GPL
+ */
+
+Ximplements(Xlocales, {
+	'empty log': 'Journal vide',
+	
+	// Extra titles
+	'title calls': 'Mods éxécutés',
+	'title debug data': 'Informations de débogage',
+	'title call messages': 'Messages des mods',
+	
+	// Extra captions
+	'caption no extra data': 'Aucune information disponible',
+	'caption infos': 'Informations',
+	
+	// Extra labels
+	'url': 'URL',
+	'response': 'Réponse du plugin',
+	'sent data': 'Données envoyées',
+	'server url': 'URL du serveur',
+	'complete url': 'URL complète',
+	
+	'call name success': 'Valides',
+	'call name warning': 'Avertissement',
+	'call name error': 'Erreur',
+	
+	'no row selected': 'Aucune entrée sélectionnée'
+});
