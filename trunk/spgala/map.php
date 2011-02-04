cca89507256efe456522f0d9444ed712d6786993
capi <capi@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-02-04 02:13:08 +0000

    [SPGala] ! tool_parse_galassia.php ! utf-8 des pages

--- trunk/spgala/map.php
+++ trunk/spgala/map.php
@@ -69,7 +69,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title></title>
 <style type="text/css">
 #container{left:40%;margin-top:-40px;padding:0;position:absolute;top:50%}
