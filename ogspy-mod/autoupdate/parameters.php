1ce020e777ed0f1e78cc6bc3f6fa2141affd7b2d
darknoon <darknoon@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-05-07 16:29:32 +0000

    Version 1.1.0 Propose la mise à jour si la version installée est différente de celle du serveur.

--- ogspy-mod/autoupdate/parameters.php
+++ ogspy-mod/autoupdate/parameters.php
@@ -4,7 +4,7 @@
 * @package [MOD] AutoUpdate
 * @author Bartheleway <contactbarthe@g.q-le-site.webou.net>
 * @version 1.1
-* created	: 25/04/2011 18:23:46 (Heure serveur)
+* created	: 07/05/2011 18:28:10 (Heure serveur)
 */
 
 if (!defined("IN_SPYOGAME")) die("Hacking attempt");
@@ -13,7 +13,7 @@ DEFINE("COADMIN", 0);
 DEFINE("AUTO_MAJ", 0);
 DEFINE("DOWNJSON", 1);
 DEFINE("CYCLE", 1);
-DEFINE("BEGIND", 25);
+DEFINE("BEGIND", 7);
 DEFINE("BEGINH", 17);
 DEFINE("MULTI", 1);
 ?>
