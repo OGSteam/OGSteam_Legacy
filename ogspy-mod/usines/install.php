0892c5f3f5e30ea740462f16bb66b40c3bb87c1e
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-05-25 07:56:51 +0000

    Ajout des functions install, uninstall et update.php Rendu comptabible au nombre de planète.

--- ogspy-mod/usines/install.php
+++ ogspy-mod/usines/install.php
@@ -1,8 +1,6 @@
 <?php
 define("IN_SPYOGAME", true);
 require_once("common.php");
-$filename = 'mod/usines/version.txt';
-if (file_exists($filename)) $file = file($filename);
-$query = "INSERT INTO ".TABLE_MOD." (id, title, menu, action, root, link, version, active) VALUES ('','Usines','Usines','usines','usines','usines.php','".trim($file[1])."','1')";
-$db->sql_query($query);
+$mod_folder = "usines";
+install_mod($mod_folder);
 ?>
--- ogspy-mod/usines/update.php
+++ ogspy-mod/usines/update.php
@@ -3,15 +3,7 @@ if (!defined('IN_SPYOGAME')) {
     die("Hacking attempt");
 }
 
-
-$filename = 'mod/usines/version.txt';
-if (file_exists($filename)) $file = file($filename);
-
-global $table_prefix;
-
-
-$query = "UPDATE ".TABLE_MOD." SET version='".trim($file[1])."' WHERE action='usines'";
-$db->sql_query($query);
-?>
-
+$mod_folder = "usines";
+$mod_name = "usines";
+update_mod($mod_folder, $mod_name);
 ?>
