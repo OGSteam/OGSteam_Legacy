01d34306594fdef75032900639dd93eaeb6b9148
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-05-30 08:06:36 +0000

    Mise a jour de compatibilité 3.0.7 + normalisation a 3 chiffres de la version des mods

--- ogspy-mod/usines/install.php
+++ ogspy-mod/usines/install.php
@@ -1,6 +1,18 @@
 <?php
 define("IN_SPYOGAME", true);
 require_once("common.php");
+global $db;
+
+$is_ok = false;
 $mod_folder = "usines";
+$is_ok = install_mod ($mod_folder);
 install_mod($mod_folder);
+if ($is_ok == true)
+	{
+		// Si besoin de creer des tables, à faire ici
+	}
+else
+	{
+  echo  "<script>alert('Désolé, un problème a eu lieu pendant l'installation, corrigez les problèmes survenue et réessayez.');</script>";
+	}
 ?>
