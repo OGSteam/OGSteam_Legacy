ab5e5f9138c2563858395157348c0ef78dac5dbf
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-05-30 07:44:23 +0000

    Mise a jour de compatibilité 3.0.7

--- ogspy-mod/autoupdate/install.php
+++ ogspy-mod/autoupdate/install.php
@@ -12,6 +12,15 @@ if (!defined('IN_SPYOGAME')) {
     die("Hacking attempt");
 }
 // Ajout du module dans la table des mod de OGSpy
+$is_ok = false;
 $mod_folder = "autoupdate";
-install_mod($mod_folder);
+$is_ok = install_mod($mod_folder);
+if ($is_ok == true)
+	{
+		//si besoin de creer des tables, a faire ici
+	}
+else
+	{
+		echo  "<script>alert('Désolé, un problème a eu lieu pendant l'installation, corrigez les problèmes survenue et réessayez.');</script>";
+	}
 ?>
