8ab3769092f5bfdc7ca9407ddde482a942c1b7c4
machine <machine@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-05-30 18:19:33 +0000

    pb d accent oki

--- ogspy-mod/cdr/lang/lang_fr.php
+++ ogspy-mod/cdr/lang/lang_fr.php
@@ -6,25 +6,25 @@
 
 $lang['menu_cdr'] = "Champs de ruines";
 $lang['menu_option'] = "Option";
-$lang['coord'] = "Coordonnées";
+$lang['coord'] = "Coordonn&eacute;es";
 $lang['nb_recy'] = "Nombre de recycleurs";
 $lang['total'] = "Total";
-$lang['metal'] = "Métal";
+$lang['metal'] = "M&eacute;tal";
 $lang['crystal'] = "Cristal";
 $lang['date'] = "Date";
 $lang['date_format'] = "d/m/Y G\hi";
 $lang['more_than'] = "+ de";
 $lang['optionof'] = "Options de "./*$mod_version*/"%s";
-$lang['tcdt'] = "Taille des Cdr à voir";
+$lang['tcdt'] = "Taille des Cdr &agrave; voir";
 $lang['colorcdr'] = "Couleur des Cdr";
 $lang['tri'] = "Choix de tri";
 $lang['ascending'] = "Croissant";
-$lang['descending'] = "Décroissant";
-$lang['galaxy'] = "Galaxie par défaut";
-$lang['update'] = "Mise à jour";
-$lang['xtense_ok'] = "Xtense ok, mod activé";
-$lang['no_xtense'] = "Le mod Xtense 2 n\'est pas installé.\nLa compatibilité du mod cdr ne sera donc pas installée !\nPensez à installer Xtense 2 c'est pratique";
-$lang['xtense_gone'] = "La compatibilité du mod cdr avec le mod Xtense2 a été désinstallée !";
-$lang['cdr_bye'] = "Merci d\'avoir utilisé cdr\nÀ bientôt !";
+$lang['descending'] = "D&eacute;croissant";
+$lang['galaxy'] = "Galaxie par d&eacute;faut";
+$lang['update'] = "Mise &agrave; jour";
+$lang['xtense_ok'] = "Xtense ok, mod activ&eacute;";
+$lang['no_xtense'] = "Le mod Xtense 2 n\'est pas install&eacute;.\nLa compatibilit&eacute; du mod cdr ne sera donc pas install&eacute;e !\nPensez à installer Xtense 2 c'est pratique";
+$lang['xtense_gone'] = "La compatibilit&eacute; du mod cdr avec le mod Xtense2 a &eacute;t&eacute; d&eacute;sinstall&eacute;e !";
+$lang['cdr_bye'] = "Merci d\'avoir utilis&eacute; cdr\nÀ bientôt !";
 
 ?>
