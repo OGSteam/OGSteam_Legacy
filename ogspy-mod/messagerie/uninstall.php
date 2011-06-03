8c0d408b12c7216e2c016eedd75f754daf2660f8
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-06-03 07:00:38 +0000

    Mise a jour de compatibilité 3.0.7 + normalisation a 3 chiffres de la version des mods

--- ogspy-mod/messagerie/uninstall.php
+++ ogspy-mod/messagerie/uninstall.php
@@ -11,26 +11,12 @@ if (!defined('IN_SPYOGAME')) {
 	die("Hacking attempt");
 	}
 
-global $table_prefix;
-
+global $db, $table_prefix;
 define("TABLE_MESSAGES", $table_prefix."messages");
 define("TABLE_MESSAGES_THREAD", $table_prefix."messages_thread");
 define("TABLE_BOARD", $table_prefix."board");
 
-
-// La suppression du mod dans la table de mod est faites par OGSpy
-// (Donc pas besoin de la faire dans le fichier uninstall.php)
-
-// Suppression des tables spécifiques au module
-// Remarque : aucune confirmation demandé... BOUM sur les messages si erreur de manip...
-// Mais pas de protocole de confirmation implémenté dans les autres modules... à réfléchir peut etre
-
-$query="DROP TABLE IF EXISTS `".TABLE_MESSAGES."`";
-$db->sql_query($query);
-
-$query="DROP TABLE IF EXISTS `".TABLE_MESSAGES_THREAD."`";
-$db->sql_query($query);
-
-$query="DROP TABLE IF EXISTS `".TABLE_BOARD."`";
-$db->sql_query($query);
+$mod_uninstall_name = "Messagerie";
+$mod_uninstall_table = TABLE_MESSAGES.','.TABLE_MESSAGES_THREAD.','.TABLE_BOARD;
+uninstall_mod ($mod_uninstall_name, $mod_uninstall_table);
 ?>
