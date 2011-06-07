36fdf11cd760e2387f176ee9a812152e20ddec40
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-06-07 07:53:13 +0000

    Erreur dans le fichier install

--- ogspy-mod/recherchealliance/install.php
+++ ogspy-mod/recherchealliance/install.php
@@ -14,7 +14,15 @@
 if (!defined('IN_SPYOGAME')) die("Hacking attempt");
 
 global $db;
-
+$is_ok = false;
 $mod_folder = "recherchealliance";
-install_mod($mod_folder);
+$is_ok = install_mod ($mod_folder);
+if ($is_ok == true)
+	{
+        // Si besoin de creer des tables, à faire ici
+	}
+else
+	{
+		echo  "<script>alert('Désolé, un problème a eu lieu pendant l'installation, corrigez les problèmes survenue et réessayez.');</script>";
+	}
 ?>
