05ad8735185f81b61b8c8c0ec4a48628a56b5f92
superbox <superbox@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-07-17 12:40:27 +0000

    fix: module adapté pour les attaques groupées

--- ogspy-mod/attaques/_xtense.php
+++ ogspy-mod/attaques/_xtense.php
@@ -49,85 +49,101 @@ $xtense_version="2.3.9";
  
 // Import des Rapports de combats
 function attack_rc($rapport)
-  {
-  global $db, $table_prefix, $attack_config, $user_data;
-  //$rapport = remove_htm($rapport["content"]);
-  //definition de la table attaques
-  define("TABLE_ATTAQUES_ATTAQUES", $table_prefix."attaques_attaques");
-  read_config();
+{
+	global $db, $table_prefix, $attack_config, $user_data;
+	//$rapport = remove_htm($rapport["content"]);
+	//definition de la table attaques
+	define("TABLE_ATTAQUES_ATTAQUES", $table_prefix."attaques_attaques");
+	read_config();
 
-  if (!$rapport['date'])
-    {
-    return FALSE;
-    }
-    else
-    {
+	if (!$rapport['date'])
+	{
+		return FALSE;
+	}
+	else
+	{
 
-		if ($rapport['win'] != 'A')
-		  {
-		  // Si l'attaquant pert alors il ne prend pas de ressources !!
-      $winmetal=0;
-      $wincristal=0;
-      $windeut=0;
-      }
-	  else {
-	  	  $winmetal=isset($rapport['result']['win_metal']) ? $rapport['result']['win_metal'] : 0;
-      $wincristal=isset($rapport['result']['win_cristal']) ? $rapport['result']['win_cristal'] : 0;
-      $windeut=isset($rapport['result']['win_deut']) ? $rapport['result']['win_deut'] : 0;
-	  }
-	
+		if ($rapport['win'] != 'A') //Si l'attaquant ne gagne pas alors il ne prend pas de ressources !
+		{
+			$winmetal=0;
+			$wincristal=0;
+			$windeut=0;
+		}
+		else
+		{
+			$winmetal=isset($rapport['result']['win_metal']) ? $rapport['result']['win_metal'] : 0;
+			$wincristal=isset($rapport['result']['win_cristal']) ? $rapport['result']['win_cristal'] : 0;
+			$windeut=isset($rapport['result']['win_deut']) ? $rapport['result']['win_deut'] : 0;
+		}
+
 		$pertes = $rapport['result']['a_lost'];
 		$timestamp = $rapport['date'];
 		
-		//Puis les informations pour les coordonnées
-		$coord_attaque = $rapport['n']['1']['coords'];
-		
-    //On vérifie que vous êtes bien l'attaquant
-		$coord_attaquant = $rapport['n']['0']['coords'];
-		//On regarde dans les coordonnées de l'espace personnel du joueur qui insère les données via le plugin si les coordonnées de l'attaquant correspondent à une de ses planètes
-    $query = "SELECT coordinates FROM ".TABLE_USER_BUILDING." WHERE user_id='".$user_data['user_id']."'";
-    $result = $db->sql_query($query);
-    $attaquant = 0;
-    $defenseur = 0;
-   	while(list($coordinates) = mysql_fetch_row($result))
-	{
-		if($coordinates == $coord_attaquant) $attaquant = 1;
-		if($coordinates == $coord_attaque) $defenseur = 1;
-	}
-	if ($attaquant != 1 && $attack_config['defenseur'] != 1) 
-    {
-    return false;
-    } 
-    else 
-    {
-    if ($defenseur == 1 && $attack_config['defenseur'] == 1)
-      {
-      // récupération des pertes défenseurs
-      $pertes = $rapport['result']['d_lost'];
-      //les coordonnées de l'attaque deviennent celle de l'attaquant
-      $coord_attaque = $coord_attaquant;
-      //on soustrait les ressources volées
-      $winmetal = -$winmetal;
-      $wincristal = -$wincristal;
-      $windeut = -$windeut;
-      }
-      
-		//On vérifie que cette attaque n'a pas déja été enregistrée
-		$query = "SELECT attack_id FROM ".TABLE_ATTAQUES_ATTAQUES." WHERE attack_user_id='".$user_data['user_id']."' AND attack_date='$timestamp' AND attack_coord='$coord_attaque' ";
-		$result = $db->sql_query($query);
-		$nb = mysql_num_rows($result);
-		
-		if ($nb == 0)
+		//Récupération des coordonnées des attaquants
+		$att=0;
+		$coords_attaquants = array();
+		while ($rapport['n'][$att]['type']!='D')
 		{
-			//On insere ces données dans la base de données
-			$query = "INSERT INTO ".TABLE_ATTAQUES_ATTAQUES." ( `attack_id` , `attack_user_id` , `attack_coord` , `attack_date` , `attack_metal` , `attack_cristal` , `attack_deut` , `attack_pertes` )
-				VALUES (
-					NULL , '".$user_data['user_id']."', '".$coord_attaque."', '".$timestamp."', '".$winmetal."', '".$wincristal."', '".$windeut."', '".$pertes."')";
-			$db->sql_query($query);
+			$coords_attaquants[$att] = $rapport['n'][$att]['coords'];
+			$att++;
 		}
-	 }
-  }
-  return TRUE;
+		//Récupération des coordonnées des défenseurs
+		$def=0;
+		$coords_defenseurs = array();
+		while ($rapport['n'][$att+$def]['type']!='A')
+		{
+			$coords_defenseurs[$def] = $rapport['n'][$att+$def]['coords'];
+			$def++;
+		}
+		
+		//Coordonnées où a eu lieu l'attaque
+		$coord_attaque = $coords_defenseurs['0'];
+		
+		//On regarde dans les coordonnées de l'espace personnel du joueur qui insère les données via le plugin si il fait partie des attaquants et/ou des défenseurs
+		$query = "SELECT coordinates FROM ".TABLE_USER_BUILDING." WHERE user_id='".$user_data['user_id']."'";
+		$result = $db->sql_query($query);
+		$attaquant = 0;
+		$defenseur = 0;
+		while((list($coordinates) = mysql_fetch_row($result)) && ($defenseur==0))
+		{
+			if(in_array($coordinates, $coords_attaquants))
+				$attaquant = 1;
+			if(in_array($coordinates, $coords_defenseurs))
+				$defenseur = 1;
+		}
+	
+		if ($attaquant != 1 && $attack_config['defenseur'] != 1) 
+		{
+			return false;
+		} 
+		else 
+		{
+			if ($defenseur == 1 && $attack_config['defenseur'] == 1)
+			{
+				//Récupération des pertes défenseurs
+				$pertes = $rapport['result']['d_lost'];
+				//On soustrait les ressources volées
+				$winmetal = -$winmetal;
+				$wincristal = -$wincristal;
+				$windeut = -$windeut;
+			}
+
+			//On vérifie que cette attaque n'a pas déja été enregistrée
+			$query = "SELECT attack_id FROM ".TABLE_ATTAQUES_ATTAQUES." WHERE attack_user_id='".$user_data['user_id']."' AND attack_date='$timestamp' AND attack_coord='$coord_attaque' ";
+			$result = $db->sql_query($query);
+			$nb = mysql_num_rows($result);
+			
+			if ($nb == 0)
+			{
+				//On insere ces données dans la base de données
+				$query = "INSERT INTO ".TABLE_ATTAQUES_ATTAQUES." ( `attack_id` , `attack_user_id` , `attack_coord` , `attack_date` , `attack_metal` , `attack_cristal` , `attack_deut` , `attack_pertes` )
+					VALUES (
+						NULL , '".$user_data['user_id']."', '".$coord_attaque."', '".$timestamp."', '".$winmetal."', '".$wincristal."', '".$windeut."', '".$pertes."')";
+				$db->sql_query($query);
+			}
+		}
+	}
+	return TRUE;
 }
 
 function attack_rr($rapport) {
