a31dbc93421ff944bf28002f0da6656af11e1f51
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-07-30 22:03:10 +0000

    Debug pour la desinstallation. Il supprimait 2 fois les tables.

--- ogspy-mod/attaques/uninstall.php
+++ ogspy-mod/attaques/uninstall.php
@@ -17,23 +17,11 @@ if (!defined('IN_SPYOGAME')) die("Hacking attempt");
 	define("TABLE_ATTAQUES_RECYCLAGES", $table_prefix."attaques_recyclages");
 	define("TABLE_ATTAQUES_ARCHIVES", $table_prefix."attaques_archives");
 	
-	//Suppression de la table attaques_attaques
-	$query = "DROP TABLE IF EXISTS ".TABLE_ATTAQUES_ATTAQUES.";";
-	$db->sql_query($query);
-	
-	//Suppression de la table attaques_recyclages
-	$query = "DROP TABLE IF EXISTS ".TABLE_ATTAQUES_RECYCLAGES.";";
-	$db->sql_query($query);
-	
-	//Suppression de la table attaques_archives
-	$query="DROP TABLE IF EXISTS ".TABLE_ATTAQUES_ARCHIVES."";
-	$db->sql_query($query);
-	
 	//Suppression des paramètres de configuration et bbcodes
 	$query="DELETE FROM ".TABLE_MOD_CFG." WHERE `mod`='Attaques'";
 	$db->sql_query($query);
 
 	$mod_uninstall_name = "attaques";
-	$mod_uninstall_table = $table_prefix."attaques_archives".', '.$table_prefix."attaques_recyclages".', '$table_prefix."attaques_attaques";
+	$mod_uninstall_table = "TABLE_ATTAQUES_ATTAQUES".', '."TABLE_ATTAQUES_RECYCLAGES".', '"TABLE_ATTAQUES_ARCHIVES";
 	uninstall_mod($mod_uninstall_name,$mod_uninstall_table);
 ?>
