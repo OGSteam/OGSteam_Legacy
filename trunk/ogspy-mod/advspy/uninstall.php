e24721cd46a1a517665cdc0246b69d7914cca2c0
darknoon <darknoon@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-09-11 13:46:37 +0000

    - Nettoyage fichier installation et uninstall

--- trunk/ogspy-mod/advspy/uninstall.php
+++ trunk/ogspy-mod/advspy/uninstall.php
@@ -1,18 +1,15 @@
 <?php
 //includes ogspy
 define("IN_SPYOGAME", true);
-require_once("common.php");
-global $db;
+
 $mod_folder= "advspy";
 //includes advspy (environement de base)
 define("IN_MOD_ADVSPY",TRUE);
 $AdvSpyConfig['Settings']['AdvSpy_BasePath']="./mod/".$mod_folder."/";
-include $AdvSpyConfig['Settings']['AdvSpy_BasePath']."Adv_config.php";
+require_once($AdvSpyConfig['Settings']['AdvSpy_BasePath']."Adv_config.php");
 
-global $table_prefix;
 $mod_uninstall_name = "advspy";
 $mod_uninstall_table = $AdvSpyConfig['Settings']['AdvSpy_TableName_RaidAlert'].', '.$AdvSpyConfig['Settings']['AdvSpy_TableName_SaveLoad'];
 uninstall_mod($mod_unistall_name,$mod_uninstall_table);
-
 
 ?>
