4f9cff6c860781688522cbf7ffe3fa0575c96360
darknoon <darknoon@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-09-11 21:20:20 +0000

    - Rectification Nom Mod

--- trunk/ogspy-mod/rechercheplus/update.php
+++ trunk/ogspy-mod/rechercheplus/update.php
@@ -3,6 +3,6 @@ if (!defined('IN_SPYOGAME')) die("Hacking Attemp!");
  
 //Fichier update
 $mod_folder = "rechercheplus";
-$mod_name = "Recherche Plus";
+$mod_name = "rechercheplus";
 update_mod($mod_folder, $mod_name);
 ?>
