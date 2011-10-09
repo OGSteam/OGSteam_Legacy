c5944c606cffcf1aea34d2fc519cb1fc28aa008a
jormund <jormund@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-10-09 11:14:16 +0000

    correction du .bat pour exclure le .sh

--- trunk/unifox/makeUnifox-tempXPI.bat
+++ trunk/unifox/makeUnifox-tempXPI.bat
@@ -1,2 +1,2 @@
 set x=UniFox-temp
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %x%.xpi * -r -mx=9 -xr!.svn -x!*.bat -x!*.xpi
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %x%.xpi * -r -mx=9 -xr!.svn -x!*.bat -x!*.xpi -x!*.sh
