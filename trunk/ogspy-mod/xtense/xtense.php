f6305a36a1dc458668cd88079ef9f2a9109bc7ab
machine <machine@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-11-26 12:31:42 +0000

    Correctif call_back classement (joueur et flotte)

--- trunk/ogspy-mod/xtense/xtense.php
+++ trunk/ogspy-mod/xtense/xtense.php
@@ -613,6 +613,7 @@ switch ($pub_type){
 						continue;
 					$query[] = '('.$timestamp.', '.$i.', "'.quote($data['player_name']).'", "'.quote($data['ally_tag']).'", '.((int)$data['points']).', '.$user_data['user_id'].')';
 					$total ++;
+                    $datas[] = $data;
 				}
 				if (!empty($query))
 					$db->sql_query('REPLACE INTO '.$table.' (datadate, rank, player, ally, points, sender_id) VALUES '.implode(',', $query));
@@ -621,13 +622,12 @@ switch ($pub_type){
 				$fields = 'datadate, rank, ally, points, sender_id, number_member, points_per_member';
 				foreach ($n as $i => $val) {//for remplacé par un foreach pour les classements erronés d'E-Univers
 					$data = $n[$i];
-					
 					$data['ally_tag'] = Check::filterSpecialChars($data['ally_tag']);
 					if(!Check::data2(isset($data['points']),
 									Check::ally_tag($data['ally_tag'])))
 						continue;
 					$query[] = '('.$timestamp.', '.$i.', "'.$data['ally_tag'].'", '.((int)$data['points']).', '.$user_data['user_id'].','.((int)$data['members']).', '.((int)$data['mean']).')';
-					
+					$datas[] = $data;
 					$total ++;
 				}
 				if (!empty($query)) {
@@ -638,7 +638,7 @@ switch ($pub_type){
 			$db->sql_query('UPDATE '.TABLE_USER.' SET rank_added_ogs = rank_added_ogs + '.$total.' WHERE user_id = '.$user_data['user_id']);
 			
 			$call->add('ranking_'.$type1.'_'.$type2, array(
-					'data' => $data,
+					'data' => $datas,
 					'offset' => $offset,
 					'time' => $time
 			));
