8210a144d9e97aaa3a5ef18340666deacf71e0e2
machine <machine@9ca476e4-2613-0410-a71a-96b0df166a37>
2011-12-05 11:42:20 +0000

    premiere partie en attente des premiers retours de testeur

--- trunk/ogspy-mod/bigbrother/include/player.php
+++ trunk/ogspy-mod/bigbrother/include/player.php
@@ -7,6 +7,11 @@ class player
     private $_status = 'x'; // seule variable qui n est pas forcement presente
     private $_date;
 
+    private $_must_update = false;
+    private $_must_historique = false;
+    private $_requete_historique;
+
+
     // constructeur privé
     private function __construct($id_player, $name_player, $id_ally, $status, $date)
     {
@@ -18,7 +23,7 @@ class player
     }
 
     // renvoi le joueur avec les informations provenant d une ligne d un systeme
-    public static function get_player_by_system($row, $date)
+    public static function get_player_by_system($row, $date, $cache)
     {
 
         // xtense envoi des id nul
@@ -29,12 +34,16 @@ class player
         $id_player = $row['player_id'];
         $name_player = $row['player_name'];
         $status = $row['status'];
+        // on créé l objet
         $instance = new player($id_player, $name_player, $id_ally, $status, $date);
+        // routine pour savoir quel update / insert faire
+        $instance->save($cache);
+
         return $instance;
     }
 
     // renvoi le joueur avec les informations provenant d une ligne d un systeme
-    public static function get_player_by_rank($row, $date)
+    public static function get_player_by_rank($row, $date, $cache)
     {
         // xtense envoi des id nul
         if (!is_numeric($row['ally_id'])) {
@@ -45,106 +54,110 @@ class player
         $name_player = $row['player_name'];
         $status = 'x'; //on ne le connait pas ...
         $instance = new player($id_player, $name_player, $id_ally, $status, $date);
+        // routine pour savoir quel update / insert faire
+        $instance->save($cache);
         return $instance;
     }
 
 
-    public function save()
+    private function save($cache)
     {
         global $db;
-        $requete = "select * from " . TABLE_PLAYER . " WHERE id = " . $this->_id_player .
-            " ;";
-        $result = $db->sql_query($requete);
-        if ($db->sql_numrows($result) == 0) {
-            // pas de resultat on ajoute le tout neuf joueur
-            $this->insert_new_player();
 
+        // var_dump($cache);
+        if (!isset($cache['id'])) {
+            $this->_must_update = true;
         } else {
-            while ($data = mysql_fetch_array($result)) {
-                // update si valeur differente
-                if ($this->_name_player != $data['name_player'] || $this->_id_ally != $data['id_ally'] ||
-                    ($this->_status != $data['status'] && $this->_status != 'x')) {
-                    // explication : ( $this->_status != $data['status'] && $this->_status != 'x')
-                    // on cnsidere qu il y a une modif pour statut que si on peut !! si maj par rank c impossible d ou le x
+            if ($this->_name_player != $cache['name_player'] || $this->_id_ally != $cache['id_ally']) {
+                // explication : ( $this->_status != $data['status'] && $this->_status != 'x')
+                // on cnsidere qu il y a une modif pour statut que si on peut !! si maj par rank c impossible d ou le x
 
-                    $this->update();
-                    $this->historique($data['name_player'], $data['id_ally'], $data['status']);
+                $this->_must_update = true;
+                $this->_must_historique = true; // on prépare l update a suivre
+                $this->historique($cache['name_player'], $cache['id_ally'], $cache['status']);
+
+            } else {
+                if ($this->_status != $cache['status'] && $cache['status'] == 'x') {
+                    $this->_must_update = true;}
+                    else 
+                    {
+                         $this->_must_update = true;
+                     $this->_must_historique = true; // on prépare l update a suivre
+                $this->historique($cache['name_player'], $cache['id_ally'], $cache['status']);
+
                 }
 
+
             }
+
+
         }
+
+
     }
 
 
     public function update_rank($type, $rank)
     {
-        global $db;
-        $bdd = null; // on selectionne la bdd
-        
-        $bdd = self::find_table_rank_player($type);
 
-        // mise a jour ( le nom est une securite supp : non necessaire)
-        $requete = "UPDATE  " . $bdd . " set   id_player = '" . $this->_id_player .
-            "'  WHERE  datadate = '" . $this->_date . "' AND rank = '" . $rank .
-            "' AND player = '" . $this->_name_player . "' ";
-        $db->sql_query($requete);
-        // var_dump($requete);
-    }
-    
 
-    public static function find_table_rank_player($type){
-         $bdd = null; // on selectionne la bdd
-        switch ($type) {
-            case 'point':
-                $bdd = TABLE_RANK_PLAYER_POINTS;
-                break;
-            case 'fleet':
-                $bdd = TABLE_RANK_PLAYER_FLEET;
-                break;
-            case 'research':
-                $bdd = TABLE_RANK_PLAYER_RESEARCH;
-                break;
-        }
-       return $bdd;
+        $retour = "(" . $this->_date . ", '" . $rank . "', '" . $this->_id_player .
+            "' )";
+        return $retour;
+
     }
-    
+
 
     // insert  les valeurs du joueur
-    private function insert_new_player()
+    public function insert_new_player()
     {
-        global $db;
+        $retour = "(" . $this->_id_player . ", '" . $this->_name_player . "', '" . $this->
+            _id_ally . "',  '" . $this->_status . "' )";
 
-        $requete = "INSERT INTO " . TABLE_PLAYER .
-            "   (id, name_player, id_ally,status)" . " VALUES (" . $this->_id_player . ", '" .
-            $this->_name_player . "', '" . $this->_id_ally . "',  '" . $this->_status .
-            "' )";
-        $db->sql_query($requete);
+        return $retour;
+
 
     }
 
     //update des valeur du joueur
-    private function update()
+    public function update()
     {
-        global $db;
 
-        $requete = "UPDATE  " . TABLE_PLAYER . " set   name_player = '" . $this->
-            _name_player . "', id_ally = '" . $this->_id_ally . "'  ,status = '" . $this->
-            _status . "'  WHERE  id = " . $this->_id_player . " ";
-        $db->sql_query($requete);
+        $this->insert_new_player();
 
     }
 
     // mise en historique de ses anciennes valeurs
     private function historique($player_name, $id_ally, $status)
     {
-        global $db;
-        $query = "INSERT INTO " . TABLE_STORY_PLAYER .
-            " (id_player, name_player, id_ally,status,datadate)" . " VALUES (" . $this->
-            _id_player . ", '" . $player_name . "', '" . $id_ally . "',  '" . $status .
-            "',  " . $this->_date . ")";
-        $db->sql_query($query);
+
+        $this->_requete_historique = "(" . $this->_id_player . ", '" . $player_name .
+            "', '" . $id_ally . "',  '" . $status . "',  " . $this->_date . ")";
 
     }
+
+
+    public function get_must_update()
+    {
+        return $this->_must_update;
+    }
+
+    public function get_must_historique()
+    {
+        return $this->_must_historique;
+    }
+
+    public function get_requete_historique()
+    {
+        return $this->_requete_historique;
+    }
+
+
 }
 
 //}
+
+
+
+
+?>
