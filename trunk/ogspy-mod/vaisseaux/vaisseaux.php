7177e27b9215afef612194740b4335260156de91
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2012-01-25 23:42:57 +0000

    -Mise jour du fichier version.txt -Comment d'un require de fichier lang

--- trunk/ogspy-mod/vaisseaux/vaisseaux.php
+++ trunk/ogspy-mod/vaisseaux/vaisseaux.php
@@ -14,7 +14,8 @@ ob_start();
 require_once("views/page_header.php");
 
 require_once("includes/ogame.php");
-require_once("parameters/lang_empire.php");
+//require_once("parameters/lang_empire.php");
+
 
 $user_empire = user_get_empire();
 $user_technology = $user_empire["technology"];
