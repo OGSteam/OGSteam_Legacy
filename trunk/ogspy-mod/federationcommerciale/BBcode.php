7988c10a1be7cc39976ca16e37552bd063fe28e7
shad <shad@9ca476e4-2613-0410-a71a-96b0df166a37>
2012-02-03 15:17:12 +0000

    - Mise a jour fichier install, uninstall et version - Débug du bb code

--- trunk/ogspy-mod/federationcommerciale/BBcode.php
+++ trunk/ogspy-mod/federationcommerciale/BBcode.php
@@ -11,42 +11,42 @@ if (!defined('IN_SPYOGAME')) {
 
 //BBcode
 	//Le haut de page et le pied de page
- 	$header = '[u][size=18][b][color=red]Échange:[/color][/b][/size][/u]';
-	$footer = 'Ce compte rendu à été fait avec le mod [url=http://www.ogsteam.fr/forums/viewtopic.php?pid=35316#p35316] Fédération commerciale [/url].';
+ 	$header = '[center][u][size=18][b][color=red]Échange:[/color][/b][/size][/u]';
+	$footer = 'Ce compte rendu à été fait avec le mod [url=http://www.ogsteam.fr/forums/viewtopic.php?pid=35316#p35316] Fédération commerciale [/url].[/center]';
 	
 	//Début du BBcode
 	$BBcode  = $header;
 		$BBcode .= "\n";
 		$BBcode .= "\n";
-		$BBcode .='<p>Vendeur<br>';
+		$BBcode .= 'Vendeur'."\n";
 		for ($i=1 ; $i<=$nb_vendeur ; $i++) {
-			$BBcode .='<br>';
-			$BBcode .='[color=red]'.$pseudo_vendeur[$i].'[/color]<br>';
-			$BBcode .='<br>';
-			$BBcode .='à envoyer<br>';
-			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.$metal_vendeur[$i].'[/color]<br>';
-			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.$cristal_vendeur[$i].'[/color]<br>';
-			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.$deuterium_vendeur[$i].'[/color]<br>';
-			$BBcode .='<br>';
-			$BBcode .='à recevoir<br>';
-			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.round($M_vendeur[$i]).'[/color]<br>';
-			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.round($C_vendeur[$i]).'[/color]<br>';
-			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.round($D_vendeur[$i]).'[/color]<br>';
+			
+			$BBcode .='[color=red]'.$pseudo_vendeur[$i].'[/color]'."\n";
+			$BBcode .= "\n";
+			$BBcode .='à envoyer'."\n";
+			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.$metal_vendeur[$i].'[/color]'."\n";
+			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.$cristal_vendeur[$i].'[/color]'."\n";
+			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.$deuterium_vendeur[$i].'[/color]'."\n";
+			$BBcode .= "\n";
+			$BBcode .='à recevoir'."\n";
+			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.round($M_vendeur[$i]).'[/color]'."\n";
+			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.round($C_vendeur[$i]).'[/color]'."\n";
+			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.round($D_vendeur[$i]).'[/color]'."\n";
+			$BBcode .= "\n";
 		}
-		$BBcode .='<p>acheteur<br>';
+		$BBcode .='Acheteur'."\n";
 		for ($i=1 ; $i<=$nb_acheteur ; $i++) {
-			$BBcode .='<br>';
-			$BBcode .='[color=red]'.$pseudo_acheteur[$i].'[/color]<br>';
-			$BBcode .='<br>';
-			$BBcode .='à envoyer<br>';
-			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.$metal_acheteur[$i].'[/color]<br>';
-			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.$cristal_acheteur[$i].'[/color]<br>';
-			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.$deuterium_acheteur[$i].'[/color]<br>';
-			$BBcode .='<br>';
-			$BBcode .='à recevoir<br>';
-			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.round($M_acheteur[$i]).'[/color]<br>';
-			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.round($C_acheteur[$i]).'[/color]<br>';
-			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.round($D_acheteur[$i]).'[/color]<br>';
+			$BBcode .='[color=red]'.$pseudo_acheteur[$i].'[/color]'."\n";
+			$BBcode .= "\n";
+			$BBcode .='à envoyer'."\n";
+			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.$metal_acheteur[$i].'[/color]'."\n";
+			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.$cristal_acheteur[$i].'[/color]'."\n";
+			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.$deuterium_acheteur[$i].'[/color]'."\n";
+			$BBcode .= "\n";
+			$BBcode .='à recevoir'."\n";
+			$BBcode .='[color=blue]Métal:[/color] [color=darkred]'.round($M_acheteur[$i]).'[/color]'."\n";
+			$BBcode .='[color=blue]cristal:[/color] [color=darkred]'.round($C_acheteur[$i]).'[/color]'."\n";
+			$BBcode .='[color=blue]deutérium:[/color] [color=darkred]'.round($D_acheteur[$i]).'[/color]'."\n";
 		}
 		$BBcode .= "\n";
     $BBcode .= $footer;
