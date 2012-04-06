83a8738b8aa0a55e8b37da2cfda93a0254d161a1
ninety <ninety@9ca476e4-2613-0410-a71a-96b0df166a37>
2012-04-06 13:35:47 +0000

    [ogspy-mod varally] Formattage des nombres et des pourcentages, correction du bug qui affichait un '+' devant un nombre negatif, probable correction de MODVARALLY-2 ([] ne contenait pas ['new'] et ['ex']), probable correction de MODVARALLY-4 (desormais on peut mettre -1 pour toutes les categories et si le nombre entré est superieur au nombre de joueurs, seul le nombre de joueurs est pris en compte), correction de la table utilisée dans la page Stats, amelioration des tri, amelioration du fieldset (pour les options du BBCode), correction d'un bug qui n'affichait pas la derniere date du rapport, amelioration de la lisibilité du code.

--- trunk/ogspy-mod/varally/include.php
+++ trunk/ogspy-mod/varally/include.php
@@ -13,7 +13,6 @@ if (!defined('IN_SPYOGAME')) die('Hacking attempt');
  */
 require_once('./parameters/id.php');
 
-
 $sql = 'SELECT `value` FROM `'.TABLE_MOD_CFG.'` WHERE `config`=\'tblAlly\'';
 $result = $db->sql_query($sql);
 list($tblSpy)=$db->sql_fetch_row($result);
@@ -27,7 +26,8 @@ list($tblSpy)=$db->sql_fetch_row($result);
  * AutoPub
  */ 
 function page_footer() {
-global $db;
+	
+	global $db;
 
 	//Récupére le numéro de version du mod
 	$request = 'SELECT `version` from `'.TABLE_MOD.'` WHERE title=\'varAlly\'';
@@ -41,13 +41,11 @@ global $db;
  * Barre des boutons
  */
 function button_bar() {
-	global $user_auth, $user_data, $pub_subaction;
+	global $user_auth, $user_data, $pub_subaction, $tblSpy;
 ?>
 	<table>
 		<tr align='center'>
 <?php
-
-global $tblSpy;
 
 		//Bouton Notre alliance
 		if ($pub_subaction != 'ally')
@@ -94,60 +92,128 @@ function check_tag ( &$tag ) {
  * @param string $player Nom du joueur
  */
 function affStats ( $field, $player ) {
+	
 	global $db;
+	
 	switch ($field) {
-		case 'points': $table = TABLE_RANK_PLAYER_POINTS; break;
 		case 'fleet': $table = TABLE_RANK_PLAYER_FLEET; break;
 		case 'research': $table = TABLE_RANK_PLAYER_RESEARCH; break;
+		default: $table = TABLE_RANK_PLAYER_POINTS; break;
 	}
-
-	$query = 'SELECT `points` FROM `'.$table.'` WHERE `player`=\''.$player.'\' ORDER BY `datadate` DESC LIMIT 2';
+	
+	$query = 'SELECT points FROM '. $table .' WHERE player = \''. $player .'\' ORDER BY datadate DESC LIMIT 2';
 	$result = $db->sql_query($query);
 	$nb = $db->sql_numrows($result);
 	
 	switch ($nb) {
-		case 0; echo '<th colspan=\'2\'> - </th>'; break;
-		case 1; (double)$val = $db->sql_fetch_assoc($result); echo '<th>? -> '.$val['points'].'</th><th>n/a</th>'; break;
-		case 2; (double)$val = $db->sql_fetch_assoc($result); $new = number_format($val['points'],0,'',''); $val = $db->sql_fetch_assoc($result); $ex = number_format($val['points'],0,'','');
-			$ecart = $new - $ex; $pourcent = round(100*$ecart/$ex,2); if ($ecart<0) { $color='red'; } elseif ($ecart>0) { $color='lime'; $ecart = '+'.$ecart; $pourcent = '+'.$pourcent; } else { $color=''; }
-			echo '<th>'.$ex.' -> '.$new.' (<font color=\''.$color.'\'>'.$ecart.'</font>)</th><th><font color=\''.$color.'\'>'.$pourcent.'%</font></th>'; break;
-		default; echo '<th colspan=\'2\'> - Error - </th>'; break;
+		case 0:
+			echo '<th colspan=\'2\'> - </th>';
+			break;
+		
+		case 1:
+			$val = $db->sql_fetch_assoc($result);
+			echo '<th>'. formate_number($val['points']) .'</th><th>-</th>';
+			break;
+		
+		case 2:
+			$val = $db->sql_fetch_assoc($result);
+			$new = $val['points'];
+			$val = $db->sql_fetch_assoc($result);
+			$ex = $val['points'];
+			$ecart = $new - $ex;
+			$pourcent = 100 * $ecart / $ex;
+			
+			if ($ecart < 0) {
+				$color = 'red';
+			}
+			elseif ($ecart > 0) {
+				$color = 'lime';
+				$ecart = '+' . $ecart;
+				$pourcent = '+'. $pourcent;
+			}
+			else {
+				$color='';
+			}
+			
+			echo '<th>'. formate_number($ex) .' -> '. formate_number($new) .' (<font color=\''. $color .'\'>'. sign($ecart) . formate_number($ecart) .'</font>)</th><th><font color=\''. $color .'\'>'. sign($pourcent) . formate_number($pourcent, 2) .'%</font></th>';
+			break;
+		
+		default:
+			echo '<th colspan=\'2\'> - Error - </th>';
+			break;
 	}
 }
 
 function affPoints ( $player, $where ) {
+	
 	global $db;
 	
-	if ($where <> '')
-	{
-		$query = 'SELECT min(`datadate`) AS `min`, max(`datadate`) as `max` FROM `'.TABLE_VARALLY.'` WHERE `player`=\''.$player.'\''.$where;
+	if ($where <> '') {
+		$query = 'SELECT min(datadate) AS min, max(datadate) as max FROM '. TABLE_VARALLY .' WHERE player = \''. $player .'\''. $where;
 		$result = $db->sql_query($query);
 		list($min, $max) = $db->sql_fetch_row($result);
-		$date = ' AND (`datadate`=\''.$min.'\' OR `datadate`=\''.$max.'\')';
+		$date = ' AND (datadate=\''. $min .'\' OR datadate=\''. $max .'\')';
 		$limit = '';
-	} else {
+	}
+	else {
 		$date = '';
 		$limit = ' LIMIT 2';
 	}
-
-	$query = 'SELECT `points` FROM `'.TABLE_VARALLY.'` WHERE `player`=\''.$player.'\''.$date.' ORDER BY `datadate` DESC'.$limit;
+	
+	$query = 'SELECT points FROM '. TABLE_VARALLY .' WHERE player = \''. $player .'\''. $date .' ORDER BY datadate DESC'. $limit;
 	$result = $db->sql_query($query);
 	$nb = $db->sql_numrows($result);
-    	switch ($nb) {
-		case 0; echo '<th colspan=\'2\'> - </th>'; break;
-		case 1; (double)$val = $db->sql_fetch_assoc($result); echo '<th>'.$val['points'].'</th><th>n/a</th>'; break;
-		case 2; (double)$val = $db->sql_fetch_assoc($result); $new = number_format($val['points'],0,'',''); $val = $db->sql_fetch_assoc($result); $ex = number_format($val['points'],0,'','');
-		    $ecart = $new - $ex; 
-		    $pourcent = round(100*$ecart/$ex,2);        
 
-		    global $tblecart; 
-		    $tblecart[] = array( "joueur" => $player, "pts" => $ecart, "prc" => $pourcent );        
+   	switch ($nb) {
+		case 0:
+			echo '<th colspan=\'2\'> - </th>';
+			break;
+		
+		case 1:
+			$val = $db->sql_fetch_assoc($result);
+			echo '<th>'. formate_number($val['points']) .'</th><th>-</th>';
+			break;
+		
+		case 2:
+			$val = $db->sql_fetch_assoc($result);
+			$new = $val['points'];
+			$val = $db->sql_fetch_assoc($result);
+			$ex = $val['points'];
+		    $ecart = $new - $ex;
+		    $pourcent = 100 * $ecart / $ex;
+		
+		    global $tblecart;
+		    $tblecart[] = array(
+				'joueur' => $player,
+				'ex'     => $ex,
+				'new'    => $new,
+				'pts'    => $ecart,
+				'prc'    => $pourcent
+			);
 
-		    if ($ecart<0) { $color='red'; } elseif ($ecart>0) { $color='lime'; $ecart = '+'.$ecart; $pourcent = '+'.$pourcent; } else { $color=''; }        
+		    if ($ecart < 0) {
+				$color = 'red';
+			}
+			elseif ($ecart > 0) {
+				$color = 'lime';
+				$ecart = '+'. $ecart;
+				$pourcent = '+'. $pourcent;
+			}
+			else {
+				$color = '';
+			}
+			
+			echo '<th>'. formate_number($ex) .' -> '. formate_number($new) .' (<font color=\''. $color .'\'>'. sign($ecart) . formate_number($ecart) .'</font>)</th><th><font color=\''. $color .'\'>'. sign($pourcent) . formate_number($pourcent, 2) .'%</font></th>';
+			break;
+		
+		default:
+			echo '<th colspan=\'2\'> - Error - </th>';
+			break;
+	}
+}
 
-		    echo '<th>'.$ex.' -> '.$new.' (<font color=\''.$color.'\'>'.$ecart.'</font>)</th><th><font color=\''.$color.'\'>'.$pourcent.'%</font></th>'; break;
-		default; echo '<th colspan=\'2\'> - Error - </th>'; break;
-	}	
+function sign ($n) {
+	return $n < 0 ? '' : '+';
 }
 
 function parseDate ( $date ) {
