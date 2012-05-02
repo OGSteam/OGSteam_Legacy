3e97f56be9c20eb7022edf8cf1b94d10ac97965a
xaviernuma <xaviernuma@9ca476e4-2613-0410-a71a-96b0df166a37>
2012-05-02 14:26:37 +0000

    Pré-remplissage du tableau. Possibilité de cacher les parties qui ne nous intéresse pas.

--- ogspy-mod/ogscalc/formule.js
+++ ogspy-mod/ogscalc/formule.js
@@ -1,73 +1,60 @@
-<!--//--><![CDATA[//><!--
-
 var batimentsOGSpy = new Array();
 var technologiesOGSpy = new Array();
 
 arrayTechno = new Array('robot','chantier','nanite','labopm','labo1','labo2','labo3','labo4','labo5','labo6','labo7','labo8','reseau');
-arrayBatiments = new Array('mine_metal','mine_cristal','synthetiseur_deuterium','centrale_solaire','reacteur_fusion','usine_robots','usine_nanites','chantier_spatial','hangar_metal','hangar_cristal','reservoir_deuterium','laboratoire','silo_missiles','terraformeur');
+arrayBatiments = new Array('mine_metal','mine_cristal','synthetiseur_deuterium','centrale_solaire','reacteur_fusion','usine_robots','usine_nanites','chantier_spatial','hangar_metal','hangar_cristal','reservoir_deuterium','laboratoire','silo_missiles','terraformeur', 'cachette_metal', 'cachette_cristal', 'cachette_deuterium');
 arrayBatimentsSpeciaux = new Array('base_lunaire','phalange_capteurs','porte_saut_spatial','depot_ravitaillement');
 arrayTechnologies = new Array('espionnage','ordinateur','armes','bouclier','protection_vaisseaux','energie','hyperespace','reacteur_combustion','reacteur_impulsion','propulsion_hyperespace','laser','ion','plasma','reseau_recherche','expeditions');
 arrayVaisseaux = new Array('pt','gt','cle','clo','cr','vb','traq','bb','dest','edlm','recycleur','vc','sonde','satellite');
 arrayDefenses = new Array('lm','ale','alo','canon_ion','gauss','lp','pb','gb','min','mip');
 arrayTotal = new Array('batiments','batiments_speciaux','technologies','vaisseaux','defenses');
 
+function $(element)
+{
+	return document.getElementById(element);
+}
+
 function writeCookie(nom, valeur)
 {
-	var argv=writeCookie.arguments;
-	var argc=writeCookie.arguments.length;
-	var expires=(argc > 2) ? argv[2] : null;
-	var path=(argc > 3) ? argv[3] : null;
-	var domain=(argc > 4) ? argv[4] : null;
-	var secure=(argc > 5) ? argv[5] : false;
-	document.cookie=nom+"="+escape(valeur)+
+	var argv = writeCookie.arguments;
+	var argc = writeCookie.arguments.length;
+	var expires = (argc > 2) ? argv[2] : null;
+	var path = (argc > 3) ? argv[3] : null;
+	var domain = (argc > 4) ? argv[4] : null;
+	var secure = (argc > 5) ? argv[5] : false;
+	
+	document.cookie = nom +"="+escape(valeur)+
 	((expires==null) ? "" : ("; expires="+expires.toGMTString()))+
 	((path==null) ? "" : ("; path="+path))+
 	((domain==null) ? "" : ("; domain="+domain))+
 	((secure==true) ? "; secure" : "");
 }
 
-function compare ( a, b )
+function compare( a, b )
 {
- return b - a;
+	return b - a;
 }
 
-function isNumeric( sText )
+function f_affichage_duree(valeur)
 {
-	var ValidChars = "0123456789";
-	var IsNumber=true;
-	var Char;
-
-	for (i = 0; i < sText.length && IsNumber == true; i++)
-	{
-		Char = sText.charAt(i);
-		if (ValidChars.indexOf(Char) == -1)
-		{
-			IsNumber = false;
-		}
-	}
-	return IsNumber;
-}
-
-function afficheDurée ( valeur )
-{
+	var texte = '-';
 	valeur = parseFloat(valeur);
-	if ( valeur != 0 )
+	if(valeur != 0)
 	{
-		var jour = Math.floor(valeur/86400);
-		valeur = valeur - 86400*jour;
-		var heure = Math.floor(valeur/3600);
-		valeur = valeur - 3600*heure;
-		var minute = Math.floor(valeur/60);
-		valeur = valeur - 60*minute;
+		var jour = Math.floor(valeur / 86400);
+		valeur = valeur - 86400 * jour;
+		var heure = Math.floor(valeur / 3600);
+		valeur = valeur - 3600 * heure;
+		var minute = Math.floor(valeur / 60);
+		valeur = valeur - 60 * minute;
 		var seconde = Math.floor(valeur);
-		var texte = '';
-		if ( jour != 0 ) { texte = jour + 'j'; }
-		if ( heure != 0 ) { texte = texte + heure + 'h'; }
-		if ( minute != 0 ) { texte = texte + minute + 'm'; }
-		if ( seconde != 0 ) { texte = texte + seconde + 's'; }
-	} else {
-		var texte = '-';
+		texte = '';
+		if ( jour != 0 ) { texte = jour + 'j '; }
+		if ( heure != 0 ) { texte += heure + 'h '; }
+		if ( minute != 0 ) { texte += minute + 'm '; }
+		if ( seconde != 0 ) { texte += seconde + 's '; }
 	}
+	
 	return texte;
 }
 
@@ -75,7 +62,7 @@ function affiche(balise)
 {
 	if (document.getElementById && document.getElementById(balise) != null)
 	{
-		document.getElementById(balise).style.display='block';
+		document.getElementById(balise).style.display = 'block';
 	}
 }
 
@@ -83,13 +70,28 @@ function cache(balise)
 {
 	if (document.getElementById && document.getElementById(balise) != null)
 	{
-		document.getElementById(balise).style.display='none';
+		document.getElementById(balise).style.display = 'none';
 	}
 }
 
 function inverse(balise)
 {
 	if ( document.getElementById(balise).style.display == 'block' ) { cache(balise); } else { affiche(balise); }
+}
+
+function f_inverse(balise, o_img)
+{
+	inverse(balise);
+	if(o_img.alt == 'moins')
+	{
+		o_img.src = document.getElementById('root').value+'plus.png';
+		o_img.alt = 'plus';
+	}
+	else
+	{
+		o_img.src = document.getElementById('root').value+'moins.png';
+		o_img.alt = 'moins';
+	}
 }
 
 function coutDe_A ( p1, alpha, i, j )
@@ -129,7 +131,7 @@ function somme ( type, arr, res )
 	if (res=='_temps')
 	{
 		document.getElementById(type+'_sec').value=result;
-		document.getElementById(type+res).value=afficheDurée(result);
+		document.getElementById(type+res).value=f_affichage_duree(result);
 	} else {
 		document.getElementById(type+res).value=result;
 	}		
@@ -162,7 +164,7 @@ function construction ( bat, met, cris, deut, coeff )
 	} else {
 		document.getElementById(sec).value=2*60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/5000/Math.pow(2,parseFloat(document.getElementById('nanite').value))/(1+parseFloat(document.getElementById('robot').value));
 	}
-	document.getElementById(temps).value=afficheDurée(parseFloat(document.getElementById(sec).value));
+	document.getElementById(temps).value=f_affichage_duree(parseFloat(document.getElementById(sec).value));
 }
 
 function batiment ( bat, met, cris, deut, coeff )
@@ -201,7 +203,7 @@ function technologie ( tech, met, cris, deut, coeff )
 	document.getElementById(cristal).value=coutDe_A(cris,coeff,parseFloat(document.getElementById(actuel).value),parseFloat(document.getElementById(voulu).value));
 	document.getElementById(deuterium).value=coutDe_A(deut,coeff,parseFloat(document.getElementById(actuel).value),parseFloat(document.getElementById(voulu).value));
 	document.getElementById(sec).value=60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/1000/(1+parseFloat(document.getElementById('laboequi').value));
-	document.getElementById(temps).value=afficheDurée(parseFloat(document.getElementById(sec).value));
+	document.getElementById(temps).value=f_affichage_duree(parseFloat(document.getElementById(sec).value));
 
 	somme('technologies',arrayTechnologies,'_metal');
 	somme('technologies',arrayTechnologies,'_cristal');
@@ -223,7 +225,7 @@ function unite ( uni, met, crist, deut )
 	document.getElementById(cristal).value=document.getElementById(voulu).value*crist;
 	document.getElementById(deuterium).value=document.getElementById(voulu).value*deut;
 	document.getElementById(sec).value=2*60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/5000/Math.pow(2,document.getElementById('nanite').value)/(1+parseFloat(document.getElementById('chantier').value));
-	document.getElementById(temps).value=afficheDurée(document.getElementById(sec).value);
+	document.getElementById(temps).value=f_affichage_duree(document.getElementById(sec).value);
 }
 
 function vaisseaux ( uni, met, crist, deut )
@@ -282,7 +284,7 @@ function rafraichiTempsBatiments ( bat )
 	temps = bat+'_temps';
 
 	document.getElementById(sec).value=2*60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/5000/Math.pow(2,document.getElementById('nanite').value)/(1+parseFloat(document.getElementById('robot').value));
-	document.getElementById(temps).value=afficheDurée(document.getElementById(sec).value);
+	document.getElementById(temps).value=f_affichage_duree(document.getElementById(sec).value);
 }
 
 function rafraichiTempsTechnologies ( bat )
@@ -294,7 +296,7 @@ function rafraichiTempsTechnologies ( bat )
 	temps = bat+'_temps';
 
 	document.getElementById(sec).value=2*60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/5000/(1+parseFloat(document.getElementById('laboequi').value));
-	document.getElementById(temps).value=afficheDurée(document.getElementById(sec).value);
+	document.getElementById(temps).value=f_affichage_duree(document.getElementById(sec).value);
 }
 
 function rafraichiTempsUnites ( bat )
@@ -306,7 +308,7 @@ function rafraichiTempsUnites ( bat )
 	temps = bat+'_temps';
 
 	document.getElementById(sec).value=2*60*60*(parseFloat(document.getElementById(metal).value)+parseFloat(document.getElementById(cristal).value))/5000/Math.pow(2,document.getElementById('nanite').value)/(1+parseFloat(document.getElementById('chantier').value));
-	document.getElementById(temps).value=afficheDurée(document.getElementById(sec).value);
+	document.getElementById(temps).value=f_affichage_duree(document.getElementById(sec).value);
 }
 
 function rafraichiRobot ()
@@ -379,11 +381,12 @@ function rafraichiChantier ()
 
 function sauvegarde ()
 {
-	var date=new Date;
-	date.setMonth(date.getMonth()+12*500);
+	var date = new Date;
+	date.setMonth(date.getMonth() + 12 * 500);
 	writeCookie("dataSave", "1", date);
 	var save = new Array();
 	var j = 0;
+	
 	for ( i = 0; i < arrayTechno.length; i++ )
 	{
 		typeUnite = arrayTechno[i];
@@ -412,6 +415,7 @@ function sauvegarde ()
 		save[j] = typeUnite+">"+valueUnite;
 		j++;
 	}
+	
 	writeCookie("data", save, date);
 	alert('Données sauvegardées');
 }
@@ -436,9 +440,9 @@ function restaure ()
 	alert('Données restaurées');
 }
 
-function resetType ( array )
+function resetType(array)
 {
-	for ( i = 0; i < array.length; i++ )
+	for(i = 0 ; i < array.length; i++)
 	{
 		bat1 = array[i]+"_actuel";
 		document.getElementById(bat1).value = 0;
@@ -457,16 +461,17 @@ function resetType ( array )
 	}
 }
 
-function resetUnite ( array )
+function resetUnite(array)
 {
-	for ( i = 0; i < array.length; i++ )
+	for(i = 0 ; i < array.length ; i++)
 	{
-		bat1 = array[i]+"_voulu";
-		bat2 = array[i]+"_metal";
-		bat3 = array[i]+"_cristal";
-		bat4 = array[i]+"_deuterium";
-		bat5 = array[i]+"_temps";
-		bat6 = array[i]+"_sec";
+		bat1 = array[i] + "_voulu";
+		bat2 = array[i] + "_metal";
+		bat3 = array[i] + "_cristal";
+		bat4 = array[i] + "_deuterium";
+		bat5 = array[i] + "_temps";
+		bat6 = array[i] + "_sec";
+		
 		document.getElementById(bat1).value = 0;
 		document.getElementById(bat2).value = 0;
 		document.getElementById(bat3).value = 0;
@@ -478,7 +483,7 @@ function resetUnite ( array )
 
 function resetTotal ()
 {
-	for ( i = 0; i < arrayTotal.length; i++ )
+	for(i = 0; i < arrayTotal.length; i++)
 	{
 		bat1 = arrayTotal[i]+"_metal";
 		bat2 = arrayTotal[i]+"_cristal";
@@ -501,7 +506,6 @@ function resetTotal ()
 
 function resetData ()
 {
-
 	document.getElementById('laboequi').value = 0;
 	
 	document.getElementById('graviton_actuel').value = 0;
@@ -522,38 +526,45 @@ function resetData ()
 	document.getElementById('total_temps').value = '-';
 }
 
-function calcul_transports ( arr )
+function calcul_transports(arr)
 {
-	for ( i = 0; i <= arr.length; i++ )
+	for(i = 0; i <= arr.length; i++)
 	{
 		if (i == arr.length)
 		{
 			type = "total";
-		} else {
+		} 
+		else
+		{
 			type = arr[i];
 		}
-		result=0;
-		result+=parseFloat(document.getElementById(type+"_metal").value);
-		result+=parseFloat(document.getElementById(type+"_cristal").value);
-		result+=parseFloat(document.getElementById(type+"_deuterium").value);
-		document.getElementById(type+"_ressources").innerHTML=formatNmb(result);
-		document.getElementById(type+"_pt").innerHTML=formatNmb(Math.ceil(result/5000));
-		document.getElementById(type+"_gt").innerHTML=formatNmb(Math.ceil(result/25000));
+		
+		result = 0;
+		result += parseFloat(document.getElementById(type + "_metal").value);
+		result += parseFloat(document.getElementById(type + "_cristal").value);
+		result += parseFloat(document.getElementById(type + "_deuterium").value);
+		
+		document.getElementById(type+"_ressources").innerHTML = formatNmb(result);
+		document.getElementById(type+"_pt").innerHTML = formatNmb(Math.ceil(result / 5000));
+		document.getElementById(type+"_gt").innerHTML = formatNmb(Math.ceil(result / 25000));
 	}
 }
+
+// Formater le nombre avec des . entre chaque groupe de 3 chiffres
 function formatNmb(numero)
-{ // Formater le nombre avec des . entre chaque groupe de 3 chiffres
+{ 
 	var nNmb = String(numero); 
-	var sRes = "";
+	var sRes = '';
 	for (var j, i = nNmb.length - 1, j = 0; i >= 0; i--, j++)
+	{
 		sRes = nNmb.charAt(i) + ((j > 0) && (j % 3 == 0)? ".": "") + sRes;
+	}
 	return sRes;
 }
 
+// Fonction qui charge les données à chaque changement dans le menu déroulant.
 function chargement(id_planete)
 {
-	// resetData();
-	
 	document.getElementById("labopm").value = batimentsOGSpy[id_planete][12];
 	document.getElementById("robot").value = batimentsOGSpy[id_planete][6];
 	document.getElementById("chantier").value = batimentsOGSpy[id_planete][8];
@@ -569,6 +580,10 @@ function chargement(id_planete)
 	{
 		document.getElementById('laboequi').value = batimentsOGSpy[id_planete][12];
 	}
+	
+	for(i = 0 ; i < arrayBatiments.length ; i++)
+	{
+		document.getElementById(arrayBatiments[i]+'_actuel').value = parseFloat(batimentsOGSpy[id_planete][i+1]);
+		document.getElementById(arrayBatiments[i]+'_voulu').value = parseFloat(batimentsOGSpy[id_planete][i+1]);
+	}
 }
-
-//--><!]]>
