93274e0a89274f0dcc889a7ce6e87bb68f786c64
machine <machine@9ca476e4-2613-0410-a71a-96b0df166a37>
2012-08-17 06:06:16 +0000

    fusion galaxy.php / galaxy_bis.php preparation séparation des fonctions ogs ( de galaxy.php vers ogs.php )

---
+++ ogspy/includes/ogs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ogs.php 
+ * Fonctions concernant le protocole de communication ogstratege / ogspy
+ * 
+ * @author Kyser
+ * @package OGSpy
+ * @subpackage includes
+ * @copyright Copyright &copy; 2007, http://ogsteam.fr/
+ * @modified $Date: 2012-07-09 16:44:26 +0200 (lun., 09 juil. 2012) $
+ * @author Kyser
+ * @link $HeadURL: http://svn.ogsteam.fr/trunk/ogspy/includes/ogs.php $
+ * @version 3.1.0 ( $Rev: 7665 $ ) 
+ */
+
+if (!defined('IN_SPYOGAME')) {
+    die("Hacking attempt");
+}
+
+
+
+
+
+
+
+
+
+?>
